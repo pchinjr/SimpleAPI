9e8ed57a05b91195dc9f94f9a0f513b8569f66fb
Paul Chin <pchinjr@gmail.com>
2016-09-29 13:44:47 -0400

    added new lines

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+//app.use('/', router);
 
 var routes = require('./routes/routes.js')(app);
+
+router.get('/', function(req, res) {
+    res.send('using Router()');
+});
+
+router.get('/api', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
 var server = app.listen(3000, function() {
     console.log("Listening on port %s...", server.address().port);
